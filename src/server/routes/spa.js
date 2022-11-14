46f05839bf17d776ed1c594e93581d04051e9e25
314@live.jp <__math>
2022-11-14 22:17:14 +0900

    reply with full html

--- src/server/routes/spa.js
+++ src/server/routes/spa.js
@@ -7,6 +7,23 @@ import { renderToString } from "react-dom/server"
 import { StaticRouter } from "react-router-dom/server";
 import React from "react";
 
+
+const Html = ({children}) => (
+<html>
+  <head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>CyberTicket</title>
+  <link rel="preload" as="image" href="/assets/images/hero.avif" />
+</head>
+<body>
+  {children}
+  <script src="/main.js"></script>
+</body>
+</html>
+);
+
 /**
  * @type {import('fastify').FastifyPluginCallback}
  */
@@ -21,16 +38,16 @@ export const spaRoute = async (fastify) => {
     throw fastify.httpErrors.notFound();
   });
 
-  fastify.get("*", (_req, res) => {
-    console.log("processing request...")
+  fastify.get("*", (_req, reply) => {
     const rendered = renderToString(
-    <StaticRouter location={_req.url}>
-      <App />
-    </StaticRouter>
+    <Html>
+      <StaticRouter location={_req.url}>
+        <App />
+      </StaticRouter>
+    </Html>
     );
-    console.log("rendered.");
 
-    return res.send(rendered);
+    reply.type("text/html").send("<!DOCTYPE html>" + rendered);
   });
 
   // fastify.get("*", (_req, reply) => {
